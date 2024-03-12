bb221d571a73f27e7337fe87d08842c1cd590c60
Linux Zade <linuxzade@gmail.com>
2024-03-12 21:15:31 +0300

    revision 01.03.0002

--- turtleSnippet.py
+++ turtleSnippet.py
@@ -5,7 +5,7 @@ import turtle
 
 # define Board and Turtle Instance
 drawingBoard = turtle.Screen()
-drawingBoard.bgcolor("light blue")
+drawingBoard.bgcolor("black")
 drawingBoard.title("Python Turtle")
 turtleInstance = turtle.Turtle()
 
@@ -29,28 +29,95 @@ for i in range(4):
 # draw star method:
 '''
 for i in range(5):  
-    turtleInstance.forward(300)
+    turtleInstance.forward(100)
     turtleInstance.left(144)
 '''
 
 # draw generic polygon func:
-'''
+
 def polyDraw(sideNum,lineDistance):
     turnAngle = 360/sideNum
     for i in range(sideNum):
         turtleInstance.forward(lineDistance)
         turtleInstance.left(turnAngle)
 
-polyDraw(5,100)
-polyDraw(6,100)
-polyDraw(7,100)
-polyDraw(8,100)
+#polyDraw(6,100)
+
+
+
+def shrinkingSquare(numOfSquare,lineLen):
+    tempLen = lineLen
+    for i in range(numOfSquare):
+        for j in range(4):
+            turtleInstance.forward(tempLen)
+            turtleInstance.left(90)
+        
+        tempLen = lineLen - (i+1)*(lineLen/numOfSquare)
+
+
+#shrinkingSquare(10,100)
+
+ 
+def drawEight(radius):
+    turtleInstance.circle(radius)
+    turtleInstance.circle(-1*radius)
+
+
+def shrinkingEight(numOfInstance):
+    turtleInstance.speed(0)
+    colorList = ["red", "purple", "blue", "green", "orange", "yellow"]
+    for i in range(numOfInstance):
+        turtleInstance.color(colorList[i%len(colorList)])
+        drawEight(2*i)
+
+def drawClover(numOfInstance):
+    shrinkingEight(numOfInstance)
+    turtleInstance.left(90)
+    shrinkingEight(numOfInstance)
+
+#drawClover(20)
+ 
+#turtle interactive draw:
+turtleInstance.color("yellow")
+
+def drawLine():
+    turtleInstance.forward(100)
+
+def rotateRight():
+    turtleInstance.right(15)
+
+def rotateLeft():
+    turtleInstance.left(15)   
+
+def clearBoard():
+    turtleInstance.clear()    
+
+def goHome():
+    turtleInstance.up()
+    turtleInstance.home()   
+    turtleInstance.down()
+
+'''
+drawingBoard.listen()
+drawingBoard.onkey(fun=drawLine, key="space")    
+drawingBoard.onkey(fun=rotateRight, key="Down")  
+drawingBoard.onkey(fun=rotateLeft, key="Up")  
+drawingBoard.onkey(fun=clearBoard, key="d")   
+drawingBoard.onkey(fun=goHome, key="h") 
 '''
 
+#--------------------------------------------------------------
+# HW : Game
+#--------------------------------------------------------------
 
 
 
-turtle.done()
+
+
+
+
+
+turtle.mainloop()
 
 
 
